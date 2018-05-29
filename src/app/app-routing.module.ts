b3964ab779cb25f0c13b1b09c3b5152257bc3c0a
unknown <glennn@mail.gvsu.edu>
2018-05-29 18:20:23 -0400

    first push with base files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+
+const routes: Routes = [
+  {path:'about', component: AboutmeComponent},
+  {path:'skills', component: SkillsComponent},
+  {path:'work', component: WorkComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'education', component: EducationComponent},
+  {path: '', redirectTo: 'about', pathMatch:'prefix'},
+  {path:'**', redirectTo:'about', pathMatch:'prefix'}
+
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, {useHash: false})
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent,
+    AboutmeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
