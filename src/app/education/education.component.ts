4e0593e5cafcfb76ca8f44fc884a48e350940799
unknown <glennn@mail.gvsu.edu>
2018-06-22 13:04:06 -0400

    work and education updates and added google maps support for work and education.

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
+
 
 @Component({
   selector: 'app-education',
@@ -9,7 +12,37 @@ export class EducationComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  
+
   ngOnInit() {
+    // var mapProp = {
+    //   center: new google.maps.LatLng(42.963983, -85.88895),
+    //   zoom: 15,
+    //   mapTypeId: google.maps.MapTypeId.ROADMAP
+    // };
+    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
+
+  mapChanger(input){
+    if (input = 1){
+    var mapProp = {
+      center: new google.maps.LatLng(42.963983, -85.88895),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+  else{
+    var mapProp = {
+      center: new google.maps.LatLng(42.668111, -83.208611),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+   }
+  }
+
 
 }
